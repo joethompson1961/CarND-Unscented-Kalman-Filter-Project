3aba8ff7575cdfc5fefc45356d0ac08d5cb92fb7
joethompson1961 <joethompson@austin.rr.com>
2017-08-04 00:32:06 -0500

    comments

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -12,83 +12,54 @@ using std::vector;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
+
+  // ukf features
   is_initialized_ = false;
   init_cnt_ = 0;
   time_step_ = 0;
-
   previous_timestamp_ = 0;
-
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
-
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
-
-  n_x_ = 5;                 // number of elements in state vector
-
-  n_aug_ = n_x_ + 2;        // number of elements in augmented state vector
-  n_sig_ = 2 * n_aug_ + 1;  // number of sigma points
-
-
-  // initial state vector
-  x_ = VectorXd(n_x_);
-
-  // initial covariance matrix
-  P_ = MatrixXd(n_x_, n_x_);
+  use_laser_ = true;          // if this is false, laser measurements will be ignored (except during init)
+  use_radar_ = true;          // if this is false, radar measurements will be ignored (except during init)
+  n_x_ = 5;                   // number of elements in state vector
+  n_aug_ = n_x_ + 2;          // number of elements in augmented state vector
+  n_sig_ = 2 * n_aug_ + 1;    // number of sigma points
+  x_ = VectorXd(n_x_);        // initial state vector
+  P_ = MatrixXd(n_x_, n_x_);  // initial covariance matrix
   P_.fill(0.0);
 
-  // Sigma point matrices
+  // ukf sigma point matrices
   Xsig_aug_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
   Xsig_pred_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
 
+  // set ukf weights
   lambda_ = 3 - n_aug_;
-
-  //set weights
   weights_ = VectorXd(2 * n_aug_ + 1);
   weights_(0) = lambda_ / (lambda_ + n_aug_);
   for (int i = 1 ; i < 2 * n_aug_ + 1 ; i++) {
     weights_(i) = 1/(2 * (lambda_ + n_aug_));
   }
 
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.775;
+  // process noise
+  std_a_ = 0.775;     // process noise standard deviation longitudinal acceleration in m/s^2
+  std_yawdd_ = 0.55;  // process noise standard deviation yaw acceleration in rad/s^2
 
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.55;
-
-  // lidar measurement dimension (p_x, p_y)
-  n_l_ = 2;
-
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
-
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
-
-  // lidar measurement covariance matrix
-  R_lidar_ = MatrixXd(n_l_,n_l_);
+  // lidar measurement features
+  n_l_ = 2;           // lidar measurement dimension (p_x, p_y)
+  std_laspx_ = 0.15;  // lidar measurement noise standard deviation position1 in m
+  std_laspy_ = 0.15;  // lidar measurement noise standard deviation position2 in m
+  R_lidar_ = MatrixXd(n_l_,n_l_); // lidar measurement covariance matrix
   R_lidar_ << std_laspx_*std_laspx_, 0,
               0,                     std_laspy_*std_laspy_;
-
-  // lidar measurement function
-  H_lidar_ = MatrixXd(2, 5);
+  H_lidar_ = MatrixXd(2, 5);  // lidar measurement function
   H_lidar_ << 1, 0, 0, 0, 0,
               0, 1, 0, 0, 0;
 
-  // Radar measurement dimension (r, phi, and r_dot)
-  n_r_ = 3;
-
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
-
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
-
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-
-  // radar measurement covariance matrix
-  R_radar_ = MatrixXd(n_r_,n_r_);
+  // radar measurement features
+  n_r_ = 3;           // radar measurement dimension (r, phi, and r_dot)
+  std_radr_ = 0.3;    // radar measurement noise standard deviation radius in m
+  std_radphi_ = 0.03; // radar measurement noise standard deviation angle in rad
+  std_radrd_ = 0.3;   // radar measurement noise standard deviation radius change in m/s
+  R_radar_ = MatrixXd(n_r_,n_r_); // radar measurement covariance matrix
   R_radar_ << std_radr_*std_radr_, 0,                       0,
               0,                   std_radphi_*std_radphi_, 0,
               0,                   0,                       std_radrd_*std_radrd_;
